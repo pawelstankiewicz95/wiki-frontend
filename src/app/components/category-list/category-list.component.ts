f3d3d3dcbc55d57cdce4dbd66744b5dc2d7487b9
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-22 00:07:36 +0100

    feat: handle shouwing categories by program id

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../models/category';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
@@ -9,4 +12,32 @@ import { Component } from '@angular/core';
 })
 export class CategoryListComponent {
 
+  public categories: Category[] = [];
+  hasProgramId: boolean = false;
+  programId: number = 1;
+
+  constructor(private categoryService: CategoryService,  private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.getCategoriesByProgramId();
+  }
+
+  public getCategoriesByProgramId(): void {
+    this.programId = +this.route.snapshot.paramMap.get('programId')!;
+    this.categoryService.getCategoriesByProgramId(this.programId).subscribe((response: Category[]) => this.categories = response);
+  }
+
+/*  public handleShowingProducts() {
+    this.hasProgramId = this.route.snapshot.paramMap.has('programId');
+    //this.hasSearchParameter = this.route.snapshot.paramMap.has('searchParam');
+    //if (this.hasSearchParameter) {
+   //   this.searchProductsByNameOrSku();
+    //}
+    if (this.hasProgramId) {
+      this.getCategoriesByProgramId();
+    }
+  //  if (!(this.hasSearchParameter) && !(this.hasCategoryId)) {
+    //  this.getProducts();
+  //  }
+  } */
 }
