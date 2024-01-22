93c26e4a0e6eb8115163b9a69763f85f99c0ad0f
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-22 23:36:32 +0100

    refactor: change name

---
+++ src/app/models/soultionSubject.ts
@@ -0,0 +1,10 @@
+import { Category } from "./category";
+
+export interface SolutionSubject {
+  id: number;
+  //user: User; // to do
+  title: string;
+  timeCreated: Date;
+  timeUpdated: Date;
+  category: Category;
+}
