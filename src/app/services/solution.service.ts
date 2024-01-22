0fccc91d7d442c85041be35eb288b42957a86b77
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-23 00:13:19 +0100

    feat: handle getting solutions

--- src/app/services/solution.service.ts
+++ src/app/services/solution.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Solution } from '../models/solution';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolutionService {
 
-  constructor() { }
+  private apiServerUrl = 'http://localhost:8080/api/solutions';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getSolutionsBySubjectId(subjectId: number): Observable<Solution[]> {
+    return this.httpClient.get<Solution[]>(`${this.apiServerUrl}/solutions-by-subject-id/${subjectId}`);
+  }
 }
