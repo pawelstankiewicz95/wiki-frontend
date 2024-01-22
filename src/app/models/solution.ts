8e71dfdac493524b24854da958ad913b9f6ddd78
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-23 00:13:03 +0100

    feat: add variables

--- src/app/models/solution.ts
+++ src/app/models/solution.ts
@@ -1,2 +1,10 @@
+import { SolutionSubject } from "./soultionSubject";
+
 export interface Solution {
+    id: number;
+    // user: User; //to do
+    subject: SolutionSubject;
+    description: string;
+    timeCreated: string;
+    timeUpdated: string;
 }
