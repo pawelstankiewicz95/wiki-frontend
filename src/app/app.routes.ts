76f6bf08caa6ba785491ec14def6c9039eee2ee6
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-23 00:10:51 +0100

    feat: add route to SolutionComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { SubjectListComponent } from './components/subject-list/subject-list.component';
+import { SolutionComponent } from './components/solution/solution.component';
 
-export const routes: Routes = [  
-{ path: 'home', component: HomeComponent },
-{ path: 'program/:programId', component: CategoryListComponent },
-{ path: 'program/:programId/category/:categoryId', component: SubjectListComponent },
-{ path: '', component: HomeComponent },
-{ path: '**', component: HomeComponent },
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'program/:programId', component: CategoryListComponent },
+    { path: 'program/:programId/category/:categoryId', component: SubjectListComponent },
+    { path: 'program/:programId/category/:categoryId/subject/:subjectId', component: SolutionComponent },
+    { path: '', component: HomeComponent },
+    { path: '**', component: HomeComponent },
 
 ];
