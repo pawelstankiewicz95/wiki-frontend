80b888d07752fc12c8dcab4c60a0c616f0e2d058
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-23 00:12:48 +0100

    style: formatting

--- src/app/models/soultionSubject.ts
+++ src/app/models/soultionSubject.ts
@@ -1,10 +1,10 @@
 import { Category } from "./category";
 
 export interface SolutionSubject {
-  id: number;
-  //user: User; // to do
-  title: string;
-  timeCreated: Date;
-  timeUpdated: Date;
-  category: Category;
+    id: number;
+    //user: User; // to do
+    title: string;
+    timeCreated: Date;
+    timeUpdated: Date;
+    category: Category;
 }
