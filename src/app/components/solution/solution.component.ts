eced48d46d45fd395570065d8614bbd4b81aa18f
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-22 23:42:35 +0100

    feat: create solution component

---
+++ src/app/components/solution/solution.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solution',
+  standalone: true,
+  imports: [],
+  templateUrl: './solution.component.html',
+  styleUrl: './solution.component.css'
+})
+export class SolutionComponent {
+
+}
