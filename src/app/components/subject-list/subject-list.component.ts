3aad576ae7b81eaaff7398cf6ead2849a1c4d3fd
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-22 23:38:26 +0100

    feat: handle getting subjects

--- src/app/components/subject-list/subject-list.component.ts
+++ src/app/components/subject-list/subject-list.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { SolutionSubject } from '../../models/soultionSubject';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { SolutionSubjectService } from '../../services/solution-subject.service';
 
 @Component({
   selector: 'app-subject-list',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './subject-list.component.html',
   styleUrl: './subject-list.component.css'
 })
 export class SubjectListComponent {
 
+  public subjects: SolutionSubject[] = [];
+  hasProgramId: boolean = false;
+  programId: number = 1;
+
+  constructor(private subjectService: SolutionSubjectService,  private route: ActivatedRoute){};
+
+  ngOnInit(): void {
+    this.getSubjectsByCategoryId();
+  }
+
+  public getSubjectsByCategoryId(): void {
+    this.programId = +this.route.snapshot.paramMap.get('categoryId')!;
+    this.subjectService.getSubjectsByCategoryId(this.programId).subscribe((response: SolutionSubject[]) => this.subjects = response);
+  }
 }
