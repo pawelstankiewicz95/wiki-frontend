82031d6bf4f1f6d73626eb9bc2d407c3f73fb926
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-24 00:28:43 +0100

    feat: add dynamic content change

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -16,10 +16,15 @@ export class CategoryListComponent {
   hasProgramId: boolean = false;
   programId: number = 1;
 
-  constructor(private categoryService: CategoryService,  private route: ActivatedRoute) {}
+  constructor(private categoryService: CategoryService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getCategoriesByProgramId();
+
+    this.route.paramMap.subscribe(params => {
+      this.programId = +params.get('programId')!
+      this.getCategoriesByProgramId();
+    });
   }
 
   public getCategoriesByProgramId(): void {
@@ -27,17 +32,17 @@ export class CategoryListComponent {
     this.categoryService.getCategoriesByProgramId(this.programId).subscribe((response: Category[]) => this.categories = response);
   }
 
-/*  public handleShowingProducts() {
-    this.hasProgramId = this.route.snapshot.paramMap.has('programId');
-    //this.hasSearchParameter = this.route.snapshot.paramMap.has('searchParam');
-    //if (this.hasSearchParameter) {
-   //   this.searchProductsByNameOrSku();
-    //}
-    if (this.hasProgramId) {
-      this.getCategoriesByProgramId();
-    }
-  //  if (!(this.hasSearchParameter) && !(this.hasCategoryId)) {
-    //  this.getProducts();
-  //  }
-  } */
+  /*  public handleShowingProducts() {
+      this.hasProgramId = this.route.snapshot.paramMap.has('programId');
+      //this.hasSearchParameter = this.route.snapshot.paramMap.has('searchParam');
+      //if (this.hasSearchParameter) {
+     //   this.searchProductsByNameOrSku();
+      //}
+      if (this.hasProgramId) {
+        this.getCategoriesByProgramId();
+      }
+    //  if (!(this.hasSearchParameter) && !(this.hasCategoryId)) {
+      //  this.getProducts();
+    //  }
+    } */
 }
