74786d76f35fecb97e0560194bb9d4d01c242bfa
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-24 23:39:43 +0100

    feat: add session storage for search input

--- src/app/components/top-nav-bar/top-nav-bar.component.ts
+++ src/app/components/top-nav-bar/top-nav-bar.component.ts
@@ -2,31 +2,36 @@ import { Component } from '@angular/core';
 import { ProgramService } from '../../services/program.service';
 import { Program } from '../../models/program';
 import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-top-nav-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './top-nav-bar.component.html',
   styleUrl: './top-nav-bar.component.css'
 })
 export class TopNavBarComponent {
 
   public programs: Program[] = [];
-
-  constructor(private programService: ProgramService, private router: Router) {}
+  searchValue: string = "";
+  constructor(private programService: ProgramService, private router: Router) { }
 
 
   ngOnInit(): void {
     this.getPrograms();
+    if (sessionStorage.getItem('searchInput') != null) {
+      this.searchValue = sessionStorage.getItem('searchInput')!;
+    }
   }
 
   public getPrograms(): void {
     this.programService.getPrograms().subscribe((response: Program[]) => this.programs = response);
   }
 
-  public onSearchSubmit(searchInput : string) {
-    this.router.navigateByUrl(`/search-subject/${searchInput}`)
+  public onSearchSubmit(searchInput: string) {
+    this.router.navigateByUrl(`/search-subject/${searchInput}`);
+    sessionStorage.setItem(searchInput, searchInput);
   }
 
 }
