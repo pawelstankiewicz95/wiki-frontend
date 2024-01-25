9296cf529982679279cfe421497b9e12a2eb9828
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-01-25 12:33:35 +0100

    chore: delete category variable

--- src/app/models/soultionSubject.ts
+++ src/app/models/soultionSubject.ts
@@ -1,10 +1,7 @@
-import { Category } from "./category";
-
 export interface SolutionSubject {
     id: number;
     //user: User; // to do
     title: string;
     timeCreated: Date;
     timeUpdated: Date;
-    category: Category;
 }
