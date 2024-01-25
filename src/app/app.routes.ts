fbf01a6917835dfaaac7409e3a43d6fa6ad6d80e
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-25 23:56:36 +0100

    feat: confiugre routes for create solution view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,29 @@ import { HomeComponent } from './components/home/home.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { SubjectListComponent } from './components/subject-list/subject-list.component';
 import { SolutionComponent } from './components/solution/solution.component';
+import { CreateSolutionViewComponent } from './components/create-solution-view/create-solution-view.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'search-subject/:searchParam', component: SubjectListComponent },
     { path: 'program/:programId', component: CategoryListComponent },
     { path: 'program/:programId/category/:categoryId', component: SubjectListComponent },
-    { path: 'program/:programId/category/:categoryId/subject/:subjectId', component: SolutionComponent },
-    { path: 'search-subject/subject/:subjectId', component: SolutionComponent },
+    {
+        path: 'search-subject/subject/:subjectId',
+        component: SolutionComponent,
+        children: [
+            { path: 'new-solution', component: CreateSolutionViewComponent },
+        ]
+    },
+    {
+        path: 'program/:programId/category/:categoryId/subject/:subjectId',
+        component: SolutionComponent,
+        children: [
+            { path: 'new-solution', component: CreateSolutionViewComponent },
+        ]
+    },
+    { path: 'new-solution', component: CreateSolutionViewComponent },
+    //{ path: 'search-subject/subject/:subjectId', component: SolutionComponent },
     { path: '', component: HomeComponent },
     { path: '**', component: HomeComponent },
 
