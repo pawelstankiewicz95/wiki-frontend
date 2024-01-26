9bd9c83bcb16be4bdfb5b4eebfeb26dcc133d309
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-01-26 11:27:14 +0100

    feat: add solutionSubjectId variable

--- src/app/models/solution.ts
+++ src/app/models/solution.ts
@@ -3,7 +3,8 @@ import { SolutionSubject } from "./soultionSubject";
 export interface Solution {
     id: number;
     // user: User; //to do
-    solutionSubject: SolutionSubject;
+    solutionSubject?: SolutionSubject;
+    solutionSubjectId?: number;
     description: string;
     timeCreated: Date;
     timeUpdated: Date;
