c7da89d720192c3e6bfe0b2fd11b888c3b0b6d31
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-30 23:28:02 +0100

    coustomize solutions and handle quill

--- src/app/components/solution/solution.component.ts
+++ src/app/components/solution/solution.component.ts
@@ -2,31 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { Solution } from '../../models/solution';
 import { SolutionService } from '../../services/solution.service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { QuillModule } from 'ngx-quill';
 
 @Component({
   selector: 'app-solution',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, QuillModule],
   templateUrl: './solution.component.html',
   styleUrl: './solution.component.css'
 })
 export class SolutionComponent implements OnInit {
   public solutions: Solution[] = [];
-  solutionId: number = 1;
+  subjectId: number = 1;
 
   constructor(private solutionService: SolutionService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       if (params.has('subjectId')) {
-        this.solutionId = +params.get('subjectId')!;
+        this.subjectId = +params.get('subjectId')!;
         this.getSolutionsBySubjectId();
       }
     });
   }
 
   public getSolutionsBySubjectId(): void {
-    this.solutionService.getSolutionsBySubjectId(this.solutionId).subscribe((response: Solution[]) => {
+    this.solutionService.getSolutionsBySubjectId(this.subjectId).subscribe((response: Solution[]) => {
       this.solutions = response;
     });
   }
