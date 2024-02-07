2523ce9481468306ce797b161afbde74e81b266b
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-07 22:21:04 +0100

    create login component with logic

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,42 @@
+// login.component.ts
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule],
+})
+export class LoginComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router) {
+
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    const val = this.form.value;
+
+    if (val.email && val.password) {
+      this.authService.login(val.email, val.password)
+        .subscribe(
+          () => {
+            console.log("User is logged in");
+            this.router.navigateByUrl('/');
+          }
+        );
+    }
+  }
+}
