5f5f3d5e76cc8ba8644bc372b88392efeb3ab9d9
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-02-09 13:56:39 +0100

    feat: create login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -21,7 +21,7 @@ export class LoginComponent {
     private router: Router) {
 
     this.form = this.fb.group({
-      email: ['', Validators.required],
+      username: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
@@ -29,8 +29,8 @@ export class LoginComponent {
   login() {
     const val = this.form.value;
 
-    if (val.email && val.password) {
-      this.authService.login(val.email, val.password)
+    if (val.username && val.password) {
+      this.authService.login(val.username, val.password)
         .subscribe(
           () => {
             console.log("User is logged in");
