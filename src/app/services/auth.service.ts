e20be8110f19bb6c81dcd4fdbe3c25d08ce2ee7d
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-13 23:06:48 +0100

    create isTokenExpired, hasValidToken, and loggedIn var and move logic from login component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,7 @@
-// auth.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { User } from '../models/user';
+import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
     providedIn: 'root'
@@ -10,12 +9,36 @@ import { User } from '../models/user';
 export class AuthService {
     private apiUrl = 'http://localhost:8080/api/auth/';
 
+    private loggedIn = new BehaviorSubject<boolean>(this.hasValidToken());
+    isLoggedIn$ = this.loggedIn.asObservable();
+  
     constructor(private http: HttpClient) { }
 
-    login(username: string, password: string): Observable<string> {
+    hasValidToken(): boolean { 
+        const token = this.getJwtToken();
+        return !!token && !this.isTokenExpired(token);
+      }
+
+      login(username: string, password: string): Observable<any> { 
         const credentials = { username, password };
-        return this.http.post<string>(`${this.apiUrl}authenticate`, credentials);
-    }
+        return this.http.post<any>(`${this.apiUrl}authenticate`, credentials).pipe(
+          tap(response => { 
+            console.log('Logged in succefully')
+            const token = response as string;
+            this.storeJwtToken(token);
+            this.loggedIn.next(true)}), 
+
+            catchError(err => {
+                if (err.status === 403) {
+                    console.error('Login failed: Invalid credentials'); 
+                    return throwError(() => new Error('Invalid credentials'));
+                } else {
+                    console.error('Unexpected login error:', err);
+                    return throwError(() => new Error('Unexpected error'));
+                }
+            })
+        );
+      }
 
     storeJwtToken(token: string): void {
         localStorage.setItem('jwtToken', token);
@@ -27,10 +50,26 @@ export class AuthService {
 
     logout(): void {
         localStorage.removeItem('jwtToken');
+         this.loggedIn.next(false);
       }
 
     isLoggedIn(): boolean {
         return !!this.getJwtToken();
     }
+
+    isTokenExpired(token: string): boolean {
+        try {
+          const decoded: any = jwtDecode(token);
+          const expirationTimestamp = decoded.exp;
+      
+          if (expirationTimestamp) {
+            return expirationTimestamp * 1000 < Date.now();
+          } else {
+            return false;
+          }
+        } catch (err) {
+          return false;
+        }
+      }
 
 }
