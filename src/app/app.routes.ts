961dffafff86f56e4c72ad0f16ec37e210767968
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-02-14 14:36:19 +0100

    feat: add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,33 +6,34 @@ import { SolutionComponent } from './components/solution/solution.component';
 import { CreateSolutionViewComponent } from './components/create-solution-view/create-solution-view.component';
 import { EditSolutionComponent } from './components/edit-solution/edit-solution.component';
 import { LoginComponent } from './components/login/login.component';
+import { authGuard } from './guard/auth-guard.guard';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'search-subject/:searchParam', component: SubjectListComponent },
-    { path: 'program/:programId', component: CategoryListComponent },
-    { path: 'program/:programId/category/:categoryId', component: SubjectListComponent },
-    { path: 'program/:programId/category/:categoryId/subject/:subjectId/edit-solution', component: EditSolutionComponent },
+    { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+    { path: 'search-subject/:searchParam', component: SubjectListComponent, canActivate: [authGuard] },
+    { path: 'program/:programId', component: CategoryListComponent, canActivate: [authGuard] },
+    { path: 'program/:programId/category/:categoryId', component: SubjectListComponent, canActivate: [authGuard] },
+    { path: 'program/:programId/category/:categoryId/subject/:subjectId/edit-solution', component: EditSolutionComponent, canActivate: [authGuard] },
     {
         path: 'search-subject/subject/:subjectId',
-        component: SolutionComponent,
+        component: SolutionComponent, canActivateChild: [authGuard],
         children: [
             { path: 'new-solution', component: CreateSolutionViewComponent },
             { path: 'edit-solution/:solutionId', component: EditSolutionComponent },
         ]
     },
-    { path: 'search-subject/subject/:subjectId/edit-solution', component: EditSolutionComponent },
+   // { path: 'search-subject/subject/:subjectId/edit-solution', component: EditSolutionComponent },
     {
         path: 'program/:programId/category/:categoryId/subject/:subjectId',
-        component: SolutionComponent,
+        component: SolutionComponent, canActivateChild: [authGuard],
         children: [
             { path: 'new-solution', component: CreateSolutionViewComponent },
        //     { path: 'edit-solution/:solutionId', component: EditSolutionComponent },
         ]
     },
-    { path: 'new-solution', component: CreateSolutionViewComponent },
-    { path: 'login', component: LoginComponent },
-    { path: '', component: HomeComponent },
-    { path: '**', component: HomeComponent },
+   // { path: 'new-solution', component: CreateSolutionViewComponent },
+    { path: 'login', component: LoginComponent},
+    { path: '', component: HomeComponent, canActivate: [authGuard]},
+    { path: '**', component: HomeComponent, canActivate: [authGuard] },
 
 ];
