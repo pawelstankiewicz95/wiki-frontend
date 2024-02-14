1777de28af0cb69783b3db0bb352e3d72c2cc01d
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-14 21:48:56 +0100

    delete unused interceptor service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { AuthInterceptor } from './services/auth-interceptor.service';
 import { authIterceptor } from './interceptors/authorization.interceptor';
 
 export const appConfig: ApplicationConfig = {
