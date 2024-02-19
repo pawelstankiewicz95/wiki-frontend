172f163575520b3e357cb337908dea88813e539c
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-19 22:08:22 +0100

    add scrolling to bottom functionallity

--- src/app/components/solution/solution.component.ts
+++ src/app/components/solution/solution.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Solution } from '../../models/solution';
 import { SolutionService } from '../../services/solution.service';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { QuillModule } from 'ngx-quill';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-solution',
   standalone: true,
-  imports: [RouterModule, QuillModule, DatePipe],
+  imports: [RouterModule, QuillModule, DatePipe, NgClass],
   templateUrl: './solution.component.html',
   styleUrl: './solution.component.css'
 })
 export class SolutionComponent implements OnInit {
   public solutions: Solution[] = [];
   subjectId: number = 1;
+  buttonHidden: boolean = false;
 
-  constructor(private solutionService: SolutionService, private route: ActivatedRoute) {}
+  constructor(private solutionService: SolutionService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -31,5 +34,19 @@ export class SolutionComponent implements OnInit {
     this.solutionService.getSolutionsBySubjectId(this.subjectId).subscribe((response: Solution[]) => {
       this.solutions = response;
     });
+  }
+
+  public add(): void {
+    this.router.navigate([`./new-solution/`], { relativeTo: this.route, queryParams: { subjectId: this.subjectId } })
+      .then(() => {
+        this.buttonHidden = true;
+        setTimeout(() => {
+          this.goToBottom();
+        }, 0);
+      });
+  }
+
+  goToBottom() {
+    window.scrollTo(0, document.body.scrollHeight);
   }
 }
