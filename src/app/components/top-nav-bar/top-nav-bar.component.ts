be15564751d51fcc2836e8bc55843e5dc9c83f85
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-26 00:21:11 +0100

    add style for home page

--- src/app/components/top-nav-bar/top-nav-bar.component.ts
+++ src/app/components/top-nav-bar/top-nav-bar.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { ProgramService } from '../../services/program.service';
 import { Program } from '../../models/program';
-import { Router, RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-top-nav-bar',
   standalone: true,
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './top-nav-bar.component.html',
   styleUrl: './top-nav-bar.component.css'
 })
@@ -17,18 +18,22 @@ export class TopNavBarComponent {
   public programs: Program[] = [];
   searchValue: string = "";
   isLoggedIn: boolean = false;
-  constructor(private programService: ProgramService, private router: Router, private authService: AuthService) { }
+  public isHome: boolean = false;
+  constructor(private programService: ProgramService, public router: Router, private authService: AuthService, private ref: ChangeDetectorRef) {
 
+  }
 
   ngOnInit(): void {
     this.isLoggedIn = this.authService.isLoggedIn();
-    
+
     this.authService.isLoggedIn$.subscribe(loggedIn => {
       this.isLoggedIn = loggedIn;
       if (loggedIn) {
         this.getPrograms();
       }
     });
+    this.isHome = this.router.url === '/home' || this.router.url === '/';
+    console.log(this.isHome);
   }
 
   public getPrograms(): void {
