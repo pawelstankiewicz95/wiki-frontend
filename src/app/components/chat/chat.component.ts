f7be2f0662374f3159308a4e587e0fe6f4b82534
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-03-12 14:08:42 +0100

    add chat logic

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Message } from '../../models/message';
+import { MessageService } from '../../services/message.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
+
+  public messages: Message[] = [];
+
+  constructor(private messageService: MessageService){}
+
+  ngOnInit(): void {
+        this.getMessages();
+
+        this.messageService.messages$.subscribe(messages => {
+          this.messages = messages;
+        });
+      }
+    
+  public getMessages(): void {
+    this.messageService.getMessages().subscribe((response: Message[]) => {
+      this.messages = response;
+    });
+  }
 
 }
