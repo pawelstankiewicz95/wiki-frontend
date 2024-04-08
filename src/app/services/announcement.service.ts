151b3e7f95e542e918da67532591f6c1e926e9e2
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-04-08 11:52:16 +0200

    create saveAnnouncement function

--- src/app/services/announcement.service.ts
+++ src/app/services/announcement.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Announcement } from '../models/announcement';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +10,24 @@ export class AnnouncementService {
 
   private apiServerUrl = 'http://localhost:8080/api/announcements';
 
+  private _announcements = new BehaviorSubject<Announcement[]>([]);
+  announcements$ = this._announcements.asObservable();
+
   constructor(private httpClient: HttpClient) { }
 
   public getAllAnnouncements(): Observable<Announcement[]> {
     return this.httpClient.get<Announcement[]>(this.apiServerUrl);
+  }
+
+  public saveAnnouncement(announcement: Announcement): Observable<Announcement> {
+
+    return this.httpClient.post<Announcement>(this.apiServerUrl, announcement)
+      .pipe(
+        tap((savedAnnouncement: Announcement) => {
+          const currentAnnouncements = this._announcements.getValue();
+          this._announcements.next([...currentAnnouncements, savedAnnouncement]);
+        })
+      );
   }
   
 }
