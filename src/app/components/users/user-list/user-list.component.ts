330dab64662eeddf72347aca4d30149e186d88fc
pawelstankiewicz <pawelsta95@gmail.com>
2024-04-12 22:45:34 +0200

    feat: handle showing user list

--- src/app/components/users/user-list/user-list.component.ts
+++ src/app/components/users/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../../../models/user';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-user-list',
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent {
+  public users: User[] = [];
 
+  constructor(private userService: UserService){};
+
+  ngOnInit(): void {
+    this.getAllUsers();
+    }
+
+  getAllUsers(){
+    this.userService.getAllUsers().subscribe((response: User[]) => this.users = response);
+  }
 }
