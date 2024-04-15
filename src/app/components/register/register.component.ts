972238425b51e06a0bb629c036f1244ec0b6e42b
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-04-15 15:12:25 +0200

    feat: add register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup , ReactiveFormsModule} from '@angular/forms';
+import { User } from '../../models/user';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ){}
+
+  ngOnInit(){
+    this.handleFormGroup();
+  }
+
+  handleFormGroup() {
+    this.registerForm = this.formBuilder.group({
+      user: this.formBuilder.group({
+        'username': new FormControl(''),
+        'email': new FormControl(''),
+        'password': new FormControl(''),
+        'firstName': new FormControl(''),
+        'lastName': new FormControl(''),
+      })
+    });
+  }
+
+  onSubmit() {
+    let user: User = this.registerForm.controls['user'].value;
+    this.authService.registerUser(user).subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error)
+    });
+  }
+
+
+
 
 }
